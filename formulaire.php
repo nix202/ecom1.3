681aee0d9676cea2a1ea33ae0cee6e9379728468
nix202 <earcanix@gmail.cm>
2023-09-29 10:57:47 -0400

    formulaire

---
+++ formulaire.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<form methode="post" action="formulaire.php">
+<h1> Formulaire </h1>
+
+<label> Nom: </label>
+<input type="text" name="Nom" id="test" size="30" maxLength="20"><br>
+<label> Prenom: </label>
+<input type="text" name="Prenom" id="test" size="30" maxLength="20"><br>
+
+</form>
